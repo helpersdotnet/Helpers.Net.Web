adb623d10b83164031300e57a8e9094c58d248d2
Prabir Shrestha <mail@prabir.me>
2010-01-15 16:07:11 +0700

    + find control recursively + todo RestartWebApplication

---
+++ src/Helpers.Net.Web/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helpers.Net.Web
+{
+    public static partial class WebHelper
+    {
+        /// <summary>
+        /// Terminates the current application. The application restarts the next time a request is recieved for it.<br/>
+        /// Requires either Full Trust or write access to web.config.
+        /// </summary>
+        /// <returns>True if successfull else false.</returns>
+        public static bool RestartWebApplication()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Helpers.Net.Web/Controls.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.UI;
+
+namespace Helpers.Net.Web
+{
+    public static partial class WebHelper
+    {
+        /// <summary>
+        /// Finds a Control Recursively.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="startingControl"></param>
+        /// <param name="id">ID of the control to search</param>
+        /// <returns></returns>
+        public static T FindControlRecursive<T>(Control startingControl, string id)
+              where T : Control
+        {
+            T found = null;
+            foreach (Control activeControl in startingControl.Controls)
+            {
+                found = activeControl as T;
+                if (found == null)
+                    found = FindControlRecursive<T>(activeControl, id);
+                else if (string.Compare(id, found.ID, true) != 0)
+                    found = null;
+                if (found != null)
+                    break;
+            }
+            return found;
+        }
+
+        /// <summary>
+        /// Finds a Control recursively.
+        /// </summary>
+        public static Control FindControlRecursive(Control Root, string Id)
+        {
+            if (Root.ID == Id)
+                return Root;
+
+            foreach (Control Ctl in Root.Controls)
+            {
+                Control FoundCtl = FindControlRecursive(Ctl, Id);
+                if (FoundCtl != null)
+                    return FoundCtl;
+            }
+
+            return null;
+
+        }
+    }
+}
