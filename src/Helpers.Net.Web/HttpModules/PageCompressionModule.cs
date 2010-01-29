25394a1ee429e7fc699f03ae532be996d624590b
Prabir Shrestha <mail@prabir.me>
2010-01-29 13:56:10 +0700

    + Page Compression Module

---
+++ src/Helpers.Net.Web/HttpModules/PageCompressionModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Web;
+using System.Web.UI;
+
+namespace Helpers.Net.Web.HttpModules
+{
+    public class PageCompressionModule : IHttpModule
+    {
+        public void Dispose() { }
+
+        public void Init(HttpApplication context)
+        {
+            context.PreRequestHandlerExecute += new EventHandler(context_PreRequestHandlerExecute);
+        }
+
+        void context_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)sender;
+
+            if (!(app.Context.CurrentHandler is Page ||
+                app.Context.CurrentHandler.GetType().Name == "SyncSessionlessHandler") ||
+                app.Request["HTTP_X_MICROSOFTAJAX"] != null)
+            {
+                return;
+            }
+
+            string acceptEncoding = app.Request.Headers["Accept-Encoding"];
+            Stream uncompressedStream = app.Response.Filter;
+
+            acceptEncoding = acceptEncoding.ToLower();
+
+            if (acceptEncoding.Contains("deflate") || acceptEncoding == "*")
+            {
+                app.Response.Filter = new DeflateStream(uncompressedStream, CompressionMode.Compress);
+                app.Response.AppendHeader("Content-Encoding", "deflate");
+            }
+            else if (acceptEncoding.Contains("gzip"))
+            {
+                app.Response.Filter = new GZipStream(uncompressedStream, CompressionMode.Compress);
+                app.Response.AppendHeader("Content-Encoding", "gzip");
+            }
+        }
+    }
+}
