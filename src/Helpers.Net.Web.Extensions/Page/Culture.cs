582abb2108bea428d3d20475ba0e092f687098b6
Prabir Shrestha <prabir@softverk.is>
2010-05-10 13:06:38 +0700

    + Helpers.Net.Web.Extensions

---
+++ src/Helpers.Net.Web.Extensions/Page/Culture.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.UI;
+
+namespace Helpers.Net.Web.Extensions
+{
+    public static partial class WebExtensions
+    {
+        public static string GetUICultureCode(this Page page)
+        {
+            return WebHelper.GetUICultureCode();
+        }
+    }
+}
